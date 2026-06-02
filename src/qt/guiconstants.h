c6778a16a33a59a2440a30c4c8bf8efacf5e67d4
Qikelela <136916805+Qikelela@users.noreply.github.com>
2026-06-02 11:04:13 +0300

    Update guiconstants.h

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -20,7 +20,7 @@ static const int STATUSBAR_ICONSIZE = 12;
 /* Transaction list -- negative amount */
 #define COLOR_NEGATIVE QColor(255, 0, 0)
 /* Transaction list -- positive amount */
-#define COLOR_POSITIVE QColor(0, 266, 0)
+#define COLOR_POSITIVE QColor(0, 255, 0)
 /* Transaction list -- bare address (without label) */
 #define COLOR_BAREADDRESS QColor(140, 140, 140)
 
